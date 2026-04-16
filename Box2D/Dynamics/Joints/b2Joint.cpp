0ba1cbca13c47cb718c1831269ecfbefd6a560aa
LaboLado <laboladoapp@gmail.com>
2026-04-17 02:39:48 +0800

    Fix crash: add e_fakeJoint case to b2Joint::Destroy and linearSlop to b2Settings

--- Box2D/Dynamics/Joints/b2Joint.cpp
+++ Box2D/Dynamics/Joints/b2Joint.cpp
@@ -182,6 +182,10 @@ void b2Joint::Destroy(b2Joint* joint, b2BlockAllocator* allocator)
 		allocator->Free(joint, sizeof(b2MotorJoint));
 		break;
 
+	case e_fakeJoint:
+		allocator->Free(joint, sizeof(b2FakeJoint));
+		break;
+
 	default:
 		b2Assert(false);
 		break;
